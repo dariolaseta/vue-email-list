f920035d399e9f5cd308891d447d47f17bbfe734
dariolaseta <dario.laseta@gmail.com>
2023-06-13 14:12:17 +0200

    created script for get emails

--- js/script.js
+++ js/script.js
@@ -5,5 +5,16 @@ createApp({
         return{
             emailArray:[]
         }
-    }
+    },
+    methods:{
+    },
+    created(){
+        for (let i = 0; i < 10; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) =>{
+                    this.emailArray.push(result.data.response);
+                });
+                }
+                
+                console.log(this.emailArray);
+        }
 }).mount("#app");
